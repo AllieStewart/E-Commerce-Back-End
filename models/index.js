f27683f8ead7e3e36ba544b5c5267ec3c5626e77
AllieStewart <megaxis1217@gmail.com>
2023-11-02 19:57:48 -0500

    edited index.js in /models, working on /routes, starting with /api/category-routes.js

--- models/index.js
+++ models/index.js
@@ -7,12 +7,39 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, 
+  {
+    foreignKey: 'product_id'
+  });
+  
 // Categories have many Products
+Category.hasMany(Products, 
+  {
+    foreignKey: 'product_id',
+    onDelete: 'CASCADE'
+  });
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, 
+  {
+    through: 
+    {
+      model: ProductTag,
+      unique: false
+    },
+    as: '' // TODO: name here
+  });
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, 
+  {
+    through: 
+    {
+      model: ProductTag,
+      unique: false
+    },
+    as: '' // TODO: name here
+  });
 
 module.exports = {
   Product,
